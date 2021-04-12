50855159b8caa3fb275413e0b933ea59564d7067
Minee <mllee@ipageon.com>
2021-04-12 13:42:45 +0900

    P228까지

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -513,7 +513,7 @@ module.exports = function (webpackEnv) {
                       includePaths: [paths.appSrc + "/styles"]
                     },
                     sourceMap: isEnvProduction && shouldUseSourceMap,
-                    prependData: `@import 'utils';`
+                    // prependData: `@import 'utils';`
                   }
                 }),
               // Don't consider CSS imports dead code even if the
